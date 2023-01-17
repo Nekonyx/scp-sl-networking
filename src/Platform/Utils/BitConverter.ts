8d1f359d8a2dbcc300657db33d43096c763715d4
Nekonyx <nekonyx@proton.me>
2023-01-17 16:47:33 +0600

    feat(platform): add checks, exceptions, and docs to BitConverter methods

--- src/Platform/Utils/BitConverter.ts
+++ src/Platform/Utils/BitConverter.ts
@@ -1,6 +1,9 @@
 import { endianness } from 'os'
 
 import { ByteSize } from '../Enums/ByteSize'
+import { ArgumentException } from '../Exceptions/ArgumentException'
+import { ArgumentNullException } from '../Exceptions/ArgumentNullException'
+import { ArgumentOutOfRangeException } from '../Exceptions/ArgumentOutOfRangeException'
 
 /** Converts base data types to an array of bytes, and an array of bytes to base data types. */
 export class BitConverter {
@@ -130,8 +133,18 @@ export class BitConverter {
    * Returns a Boolean value converted from the byte at a specified position in a byte array.
    * @param bytes A byte array.
    * @param startIndex The index of the byte within value to convert.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToBoolean(bytes: Buffer, startIndex: number): boolean {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
     return bytes[startIndex] === 1
   }
 
@@ -139,9 +152,26 @@ export class BitConverter {
    * Returns a Unicode character converted from two bytes at a specified position in a byte array.
    * @param bytes An array that includes the two bytes to convert.
    * @param startIndex The index of the first byte to convert.
-   * @returns
+   * @throws {ArgumentException} startIndex equals the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToChar(bytes: Buffer, startIndex: number): string {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex === bytes.length - 1) {
+      throw new ArgumentException(
+        'Value equals the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes
       .subarray(startIndex, startIndex + ByteSize.Char)
       .toString('utf16le')
@@ -151,8 +181,26 @@ export class BitConverter {
    * Returns a double-precision floating point number converted from eight bytes at a specified position in a byte array.
    * @param bytes An array of bytes that includes the eight bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToDouble(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 7 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readDoubleLE(startIndex)
   }
 
@@ -160,8 +208,26 @@ export class BitConverter {
    * Returns a 16-bit signed integer converted from two bytes at a specified position in a byte array.
    * @param bytes An array of bytes that includes the two bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex equals the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToInt16(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex === bytes.length - 1) {
+      throw new ArgumentException(
+        'Value equals the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readInt16LE(startIndex)
   }
 
@@ -169,8 +235,26 @@ export class BitConverter {
    * Returns a 32-bit signed integer converted from four bytes at a specified position in a byte array.
    * @param bytes An array of bytes that includes the four bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToInt32(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 3 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readInt32LE(startIndex)
   }
 
@@ -178,8 +262,26 @@ export class BitConverter {
    * Returns a 64-bit signed integer converted from eight bytes at a specified position in a byte array.
    * @param bytes An array of bytes that includes the eight bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToInt64(bytes: Buffer, startIndex: number): bigint {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 7 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readBigInt64LE(startIndex)
   }
 
@@ -187,29 +289,109 @@ export class BitConverter {
    * Returns a single-precision floating point number converted from four bytes at a specified position in a byte array.
    * @param bytes An array of bytes.
    * @param startIndex The starting position within value.
-   * @returns
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToSingle(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 3 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readFloatLE(startIndex)
   }
 
   /**
    * Converts the numeric value of each element of a specified array of bytes to its equivalent hexadecimal string representation.
    * @param bytes An array of bytes.
+   * @throws {ArgumentNullException} value is null.
    */
   public static ToString(bytes: Buffer): string
+  /**
+   * Converts the numeric value of each element of a specified subarray of bytes to its equivalent hexadecimal string representation.
+   * @param bytes An array of bytes.
+   * @param startIndex The starting position within value.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
+   */
   public static ToString(bytes: Buffer, startIndex: number): string
-  public static ToString(bytes: Buffer, startIndex: number, length: number): string
-  public static ToString(bytes: Buffer, startIndex?: number, length?: number): string {
-    return bytes.toString('hex')
+  /**
+   * Converts the numeric value of each element of a specified subarray of bytes to its equivalent hexadecimal string representation.
+   * @param bytes An array of bytes that includes the bytes to convert.
+   * @param startIndex The starting position within value.
+   * @param length The number of array elements in value to convert.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex or length is less than zero.
+   * @throws {ArgumentOutOfRangeException} startIndex is greater than zero and is greater than or equal to the length of value.
+   * @throws {ArgumentException} The combination of startIndex and length does not specify a position within value; that is, the startIndex parameter is greater than the length of value minus the length parameter.
+   */
+  public static ToString(
+    bytes: Buffer,
+    startIndex: number,
+    length: number
+  ): string
+  public static ToString(
+    bytes: Buffer,
+    startIndex?: number,
+    length?: number
+  ): string {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex && (startIndex < 0 || startIndex > bytes.length - 1)) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (length && length < 0) {
+      throw new ArgumentOutOfRangeException('length')
+    }
+
+    if (startIndex && length && startIndex > bytes.length - length) {
+      throw new ArgumentException(
+        'The combination of startIndex and length does not specify a position within value; that is, the startIndex parameter is greater than the length of value minus the length parameter.',
+        'startIndex'
+      )
+    }
+
+    return this.bufferToHexString(bytes)
   }
 
   /**
    * Returns a 16-bit unsigned integer converted from two bytes at a specified position in a byte array.
    * @param bytes The array of bytes that includes the two bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex equals the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToUInt16(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex === bytes.length - 1) {
+      throw new ArgumentException(
+        'Value equals the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readUInt16LE(startIndex)
   }
 
@@ -217,8 +399,26 @@ export class BitConverter {
    * Returns a 32-bit unsigned integer converted from four bytes at a specified position in a byte array.
    * @param bytes An array of bytes.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToUInt32(bytes: Buffer, startIndex: number): number {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 3 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 3, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readUInt32LE(startIndex)
   }
 
@@ -226,8 +426,41 @@ export class BitConverter {
    * Returns a 64-bit unsigned integer converted from eight bytes at a specified position in a byte array.
    * @param bytes An array of bytes that includes the eight bytes to convert.
    * @param startIndex The starting position within value.
+   * @throws {ArgumentException} startIndex is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.
+   * @throws {ArgumentNullException} value is null.
+   * @throws {ArgumentOutOfRangeException} startIndex is less than zero or greater than the length of value minus 1.
    */
   public static ToUInt64(bytes: Buffer, startIndex: number): bigint {
+    if (!bytes) {
+      throw new ArgumentNullException('bytes')
+    }
+
+    if (startIndex < 0 || startIndex > bytes.length - 1) {
+      throw new ArgumentOutOfRangeException('startIndex')
+    }
+
+    if (startIndex >= bytes.length - 7 && startIndex <= bytes.length - 1) {
+      throw new ArgumentException(
+        'Value is greater than or equal to the length of value minus 7, and is less than or equal to the length of value minus 1.',
+        'startIndex'
+      )
+    }
+
     return bytes.readBigUInt64LE(startIndex)
+  }
+
+  /**
+   * Converts the numeric value of each element of a specified subarray of bytes to its equivalent hexadecimal string representation.
+   * @param value A buffer.
+   * @returns A string of hexadecimal pairs separated by hyphens, where each pair represents the corresponding element in a subarray of value; for example, "7F-2C-4A-00".
+   */
+  private static bufferToHexString(value: Buffer): string {
+    const parts: string[] = Array(value.length)
+
+    for (let i = 0; i < value.length; i++) {
+      parts[i] = value[i].toString(16).padStart(2, '0').toUpperCase()
+    }
+
+    return parts.join('-')
   }
 }
